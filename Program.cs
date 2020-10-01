6df0bd16e8c79b9ac5bd523d1305bf86365fd4fe
KageKirin <kagekirin@gmail.com>
2020-10-01 21:02:39 +0900

    Add CLI params

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.IO;
+using System.CommandLine.DragonFruit;
 
 namespace AssemblyModder
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		/// <summary>
+		/// Modifies symbol access in given assembly of given symbols to public
+		/// </summary>
+		/// <param name="inputAssembly">Assembly to modify</param>
+		/// <param name="outputAssembly">Assembly to save</param>
+		/// <param name="symbol">Symbol to make public</param>
+		static void Main(FileInfo inputAssembly, FileInfo outputAssembly, string symbol)
+		{
+			Console.WriteLine($"{inputAssembly} -> publish {symbol} -> {outputAssembly}");
+		}
+	}
 }
