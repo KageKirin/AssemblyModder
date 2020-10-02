0f84d5858b664154d60f26485a8cbd37566e2876
KageKirin <kagekirin@gmail.com>
2020-10-02 09:57:22 +0900

    Implement injection of [InternalsVisibleTo] attribute to given internal types (NON FUNCTIONAL)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.IO;
 using System.CommandLine.DragonFruit;
+using Mono.Cecil;
 
 namespace AssemblyModder
 {
@@ -12,9 +15,37 @@ namespace AssemblyModder
 		/// <param name="inputAssembly">Assembly to modify</param>
 		/// <param name="outputAssembly">Assembly to save</param>
 		/// <param name="symbol">Symbol to make public</param>
-		static void Main(FileInfo inputAssembly, FileInfo outputAssembly, string symbol)
+		/// <param name="internalsVisibleToAssembly">Assembly to make internals visible to using "[InternalsVisibleTo]"</param>
+		static void Main(FileInfo inputAssembly, FileInfo outputAssembly, string symbol, string internalsVisibleToAssembly)
 		{
 			Console.WriteLine($"{inputAssembly} -> publish {symbol} -> {outputAssembly}");
+
+			ModuleDefinition module = ModuleDefinition.ReadModule(inputAssembly.FullName, new ReaderParameters());
+			foreach (TypeDefinition type in module.Types)
+			{
+				Console.WriteLine($"{type.FullName} ({type.Attributes})");
+
+				if (type.FullName == symbol)
+				{
+					Console.WriteLine($"overwriting {type.FullName} ({type.Attributes})");
+					//type.IsNotPublic = false;
+					//type.IsPublic = true;
+
+					var customAttr = new CustomAttribute(
+						module.Import(
+							typeof(InternalsVisibleToAttribute).GetConstructor(
+								new[] {typeof(string)}
+							)
+						)
+					);
+
+					customAttr.ConstructorArguments.Add(new CustomAttributeArgument(module.TypeSystem.String, internalsVisibleToAssembly));
+					module.Assembly.CustomAttributes.Add(customAttr);
+					Console.WriteLine($"{type.FullName} ({type.Attributes})");
+				}
+			}
+
+			module.Write(outputAssembly.FullName, new WriterParameters());
 		}
 	}
 }
